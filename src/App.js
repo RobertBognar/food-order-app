3475383784207e2553c75d86416431c1d1136a37
RobertBognar <robertbognar2@gmail.com>
2021-12-03 13:24:21 +0100

    updating header, fixing sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+
 import './App.css';
 function App() {
     return (
-        <div className="App">
-            <h2>TEST</h2>
-        </div>
+        <Fragment>
+            <Header />
+        </Fragment>
     );
 }
 
