36b815669d4325b46dbe620d9e816e8d3051b889
RobertBognar <robertbognar2@gmail.com>
2021-12-04 22:19:31 +0100

    listing files, fixing responsivnes, variables, sass, adding overlay modal-need to remove it, adding cart items & sass

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React, { Fragment } from 'react';
 import Header from './components/Layout/Header';
 
 import './App.css';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 function App() {
     return (
         <Fragment>
+            <Cart />
             <Header />
+            <main>
+                <Meals />
+            </main>
         </Fragment>
     );
 }
