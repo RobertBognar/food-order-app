923af7b9562d6c2dff6da8488e88e623c11cefe1
RobertBognar <robertbognar2@gmail.com>
2021-12-07 15:13:59 +0100

    adding form on order to fill information, need to fix cart send to database and fix responsivnes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import CartContext from '../../store/cart-context';
 import classes from './Cart.module.css';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+    const [isCheckout, setIsCheckout] = useState(false);
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -19,6 +21,23 @@ const Cart = (props) => {
     const cartItemAddHandler = (item) => {
         cartCtx.addItem({ ...item, amount: 1 });
     };
+
+    const orderHandler = () => {
+        setIsCheckout(true);
+    };
+
+    const modalActions = (
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onClose}>
+                Close
+            </button>
+            {hasItems && (
+                <button className={classes.button} onClick={orderHandler}>
+                    Order
+                </button>
+            )}
+        </div>
+    );
 
     const cartItems = (
         <ul className={classes['cart-items']}>
@@ -41,15 +60,8 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button
-                    className={classes['button--alt']}
-                    onClick={props.onClose}
-                >
-                    Close
-                </button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onCancel={props.onClose} />}
+            {!isCheckout && modalActions}
         </Modal>
     );
 };
---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,116 @@
+import { useRef, useState } from 'react';
+
+import classes from './Checkout.module.css';
+
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim() === 5;
+
+const Checkout = (props) => {
+    const [formInputsValidity, setFormInputsValidity] = useState({
+        name: true,
+        street: true,
+        city: true,
+        postalCode: true,
+    });
+
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalCodeInputRef = useRef();
+    const cityInputRef = useRef();
+
+    const confirmHandler = (event) => {
+        event.preventDefault();
+
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
+
+        const enteredNameIsValid = !isEmpty(enteredName);
+        const enteredStreetIsValid = !isEmpty(enteredStreet);
+        const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+        const enteredCityIsValid = !isEmpty(enteredCity);
+
+        setFormInputsValidity({
+            name: enteredNameIsValid,
+            street: enteredStreetIsValid,
+            city: enteredCityIsValid,
+            postalCode: enteredPostalCodeIsValid,
+        });
+
+        const formIsValid =
+            enteredNameIsValid &&
+            enteredStreetIsValid &&
+            enteredCityIsValid &&
+            enteredPostalCodeIsValid;
+
+        if (!formIsValid) {
+            return;
+        }
+    };
+
+    const nameControllClasses = `${classes.control} ${
+        formInputsValidity.name ? '' : classes.invalid
+    }`;
+
+    const streetControllClasses = `${classes.control} ${
+        formInputsValidity.street ? '' : classes.invalid
+    }`;
+
+    const postalCodeControllClasses = `${classes.control} ${
+        formInputsValidity.postalCode ? '' : classes.invalid
+    }`;
+
+    const cityControllClasses = `${classes.control} ${
+        formInputsValidity.city ? '' : classes.invalid
+    }`;
+
+    return (
+        <form className={classes.form} onSubmit={confirmHandler}>
+            <div className={nameControllClasses}>
+                <label htmlFor="name">Your Name</label>
+                <input type="text" id="name" ref={nameInputRef} />
+                {!formInputsValidity.name && (
+                    <p className={classes['pop-message']}>
+                        Please enter your name.
+                    </p>
+                )}
+            </div>
+            <div className={streetControllClasses}>
+                <label htmlFor="street">Street</label>
+                <input type="text" id="street" ref={streetInputRef} />
+                {!formInputsValidity.street && (
+                    <p className={classes['pop-message']}>
+                        Please enter your street address.
+                    </p>
+                )}
+            </div>
+            <div className={postalCodeControllClasses}>
+                <label htmlFor="postal">Postal Code</label>
+                <input type="text" id="postal" ref={postalCodeInputRef} />
+                {!formInputsValidity.postalCode && (
+                    <p className={classes['pop-message']}>
+                        Please enter city postal code.
+                    </p>
+                )}
+            </div>
+            <div className={cityControllClasses}>
+                <label htmlFor="city">City</label>
+                <input type="text" id="city" ref={cityInputRef} />
+                {!formInputsValidity.city && (
+                    <p className={classes['pop-message']}>
+                        Please enter city name.
+                    </p>
+                )}
+            </div>
+            <div className={classes.actions}>
+                <button type="button" onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
+        </form>
+    );
+};
+
+export default Checkout;
