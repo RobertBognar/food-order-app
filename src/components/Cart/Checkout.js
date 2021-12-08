177f96f49fcd5a3ea28914570d208352c2d86d83
RobertBognar <robertbognar2@gmail.com>
2021-12-08 09:26:56 +0100

    enabling orders via firebase backend

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 import classes from './Checkout.module.css';
 
 const isEmpty = (value) => value.trim() === '';
-const isFiveChars = (value) => value.trim() === 5;
+const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
     const [formInputsValidity, setFormInputsValidity] = useState({
@@ -28,8 +28,8 @@ const Checkout = (props) => {
 
         const enteredNameIsValid = !isEmpty(enteredName);
         const enteredStreetIsValid = !isEmpty(enteredStreet);
-        const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
         const enteredCityIsValid = !isEmpty(enteredCity);
+        const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
 
         setFormInputsValidity({
             name: enteredNameIsValid,
@@ -47,59 +47,64 @@ const Checkout = (props) => {
         if (!formIsValid) {
             return;
         }
+
+        // Submit cart data
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            city: enteredCity,
+            postalCode: enteredPostalCode,
+        });
     };
 
-    const nameControllClasses = `${classes.control} ${
+    const nameControlClasses = `${classes.control} ${
         formInputsValidity.name ? '' : classes.invalid
     }`;
-
-    const streetControllClasses = `${classes.control} ${
+    const streetControlClasses = `${classes.control} ${
         formInputsValidity.street ? '' : classes.invalid
     }`;
-
-    const postalCodeControllClasses = `${classes.control} ${
+    const postalCodeControlClasses = `${classes.control} ${
         formInputsValidity.postalCode ? '' : classes.invalid
     }`;
-
-    const cityControllClasses = `${classes.control} ${
+    const cityControlClasses = `${classes.control} ${
         formInputsValidity.city ? '' : classes.invalid
     }`;
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
-            <div className={nameControllClasses}>
+            <div className={nameControlClasses}>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" ref={nameInputRef} />
                 {!formInputsValidity.name && (
                     <p className={classes['pop-message']}>
-                        Please enter your name.
+                        Please enter a valid name!
                     </p>
                 )}
             </div>
-            <div className={streetControllClasses}>
+            <div className={streetControlClasses}>
                 <label htmlFor="street">Street</label>
                 <input type="text" id="street" ref={streetInputRef} />
                 {!formInputsValidity.street && (
                     <p className={classes['pop-message']}>
-                        Please enter your street address.
+                        Please enter a valid street!
                     </p>
                 )}
             </div>
-            <div className={postalCodeControllClasses}>
+            <div className={postalCodeControlClasses}>
                 <label htmlFor="postal">Postal Code</label>
                 <input type="text" id="postal" ref={postalCodeInputRef} />
                 {!formInputsValidity.postalCode && (
                     <p className={classes['pop-message']}>
-                        Please enter city postal code.
+                        Please enter a valid postal code (5 characters long)!
                     </p>
                 )}
             </div>
-            <div className={cityControllClasses}>
+            <div className={cityControlClasses}>
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" ref={cityInputRef} />
                 {!formInputsValidity.city && (
                     <p className={classes['pop-message']}>
-                        Please enter city name.
+                        Please enter a valid city!
                     </p>
                 )}
             </div>
